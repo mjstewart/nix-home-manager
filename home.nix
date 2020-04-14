34949ecee7b0582100a84733c23b56b1984ea7c7
Matt Stewart <smtp.matt@gmail.com>
2020-04-15 08:21:07 +1000

    update vscode extensions

--- home.nix
+++ home.nix
@@ -14,6 +14,7 @@ let
     hoogle
     dhall
     cabal-install
+    ormolu
   ];
 
   # tell ghc to register these additional libraries
--- overlays.nix
+++ overlays.nix
@@ -8,38 +8,38 @@ self: super:
     ] ++ self.vscode-utils.extensionsFromVscodeMarketplace [
         {
           name = "vim";
-          version = "1.11.3";
+          version = "1.13.1";
           publisher = "vscodevim";
-          # nix-prefetch-url https://vscodevim.gallery.vsassets.io/_apis/public/gallery/publisher/vscodevim/extension/vim/1.11.3/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
-          sha256 = "1smzsgcrkhghbnpy51gp28kh74l7y4s2m8pfxabb4ffb751254j0";
+          # nix-prefetch-url https://vscodevim.gallery.vsassets.io/_apis/public/gallery/publisher/vscodevim/extension/vim/1.13.1/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
+          sha256 = "04s0myx4j6ip86ic78cmaj8mb7wds4pmlj2fjy3wmyrbcyadn4h4";
         }
         {
           name = "vscode-docker";
-          version = "0.8.1";
+          version = "1.0.0";
           publisher = "ms-azuretools";
-          # nix-prefetch-url https://ms-azuretools.gallery.vsassets.io/_apis/public/gallery/publisher/ms-azuretools/extension/vscode-docker/0.8.1/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
-          sha256 = "0n59whmcrx8946xix6skvc50f2vsc85ckvn8cs06w9mqmymm1q0s";
+          # nix-prefetch-url https://ms-azuretools.gallery.vsassets.io/_apis/public/gallery/publisher/ms-azuretools/extension/vscode-docker/1.0.0/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
+          sha256 = "1zljdgym3kz4plb2a3z0yxvpqf4lnf215rajjs5sr7dxx3dwrxdg";
         }
         {
           name = "vscode-xml";
-          version = "0.9.1";
+          version = "0.11.0";
           publisher = "redhat";
-          # nix-prefetch-url https://redhat.gallery.vsassets.io/_apis/public/gallery/publisher/redhat/extension/vscode-xml/0.9.1/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
-          sha256 = "12nl2wm01y6kv4csnfz8hgkf227k5vkpnzcnfyr8cxd11wkb5nna";
+          # nix-prefetch-url https://redhat.gallery.vsassets.io/_apis/public/gallery/publisher/redhat/extension/vscode-xml/0.11.0/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
+          sha256 = "12z3ls6rhkf7b5qqs3r5n4vk9lzq90cj7f6d1r4ywraij82lngs5";
         }
         {
           name = "vscode-proto3";
-          version = "0.3.0";
+          version = "0.4.2";
           publisher = "zxh404";
-          # nix-prefetch-url https://zxh404.gallery.vsassets.io/_apis/public/gallery/publisher/zxh404/extension/vscode-proto3/0.3.0/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
-          sha256 = "1iylw9hihqz0pab4iisykgrq20141v5f1r6l4cif1z4237nd3z60";
+          # nix-prefetch-url https://zxh404.gallery.vsassets.io/_apis/public/gallery/publisher/zxh404/extension/vscode-proto3/0.4.2/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
+          sha256 = "05da62iahnnjxkgdav14c1gn90lkgyk9gc5rardsqijx2x6dgjn0";
         }
         {
           name = "daml";
-          version = "0.13.46";
+          version = "0.13.55";
           publisher = "digitalassetholdingsllc";
-          # nix-prefetch-url https://digitalassetholdingsllc.gallery.vsassets.io/_apis/public/gallery/publisher/digitalassetholdingsllc/extension/daml/0.13.46/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
-          sha256 = "08km1cjhari2wqjmwsc80vz8kf3sx8ddlrm2n6wimi1lz0kf12v2";
+          # nix-prefetch-url https://digitalassetholdingsllc.gallery.vsassets.io/_apis/public/gallery/publisher/digitalassetholdingsllc/extension/daml/0.13.55/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
+          sha256 = "10wd6mnjkr2slm7bzz9mwri9lqyzsmjb77s1jyhj3rfphlghbz4m";
         }
         {
           name = "dhall-lang";
@@ -50,10 +50,10 @@ self: super:
         }
         {
           name = "haskell-ghcid";
-          version = "0.2.1";
+          version = "0.3.0";
           publisher = "ndmitchell";
-          # nix-prefetch-url https://ndmitchell.gallery.vsassets.io/_apis/public/gallery/publisher/ndmitchell/extension/haskell-ghcid/0.2.1/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
-          sha256 = "16ibf8ckcmsq000qlvn34ki16qxyvd2wcd76kpjbfk6p3jfcakca";
+          # nix-prefetch-url https://ndmitchell.gallery.vsassets.io/_apis/public/gallery/publisher/ndmitchell/extension/haskell-ghcid/0.3.0/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
+          sha256 = "029dwi0y5srn3s0d70m95lbn0jng22rnhcmx14s13dmh34hzm5vb";
         }
         {
           name = "better-align";
@@ -61,6 +61,13 @@ self: super:
           publisher = "wwm";
           # nix-prefetch-url https://wwm.gallery.vsassets.io/_apis/public/gallery/publisher/wwm/extension/better-align/1.1.6/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
           sha256 = "1ldvpava9xlqy3zwwc0c04pk9dh09jwcwz5lk3b2cr1z8bxn54lh";
+        }
+        {
+          name = "ormolu-vscode";
+          version = "0.0.5";
+          publisher = "sjurmillidahl";
+          # nix-prefetch-url https://sjurmillidahl.gallery.vsassets.io/_apis/public/gallery/publisher/sjurmillidahl/extension/ormolu-vscode/0.0.5/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
+          sha256 = "1yanvj50cg7n4sgfmbhmn74kfb3sqwfxmi67pjkimizd72cggi3r";
         }
       ];
    };
